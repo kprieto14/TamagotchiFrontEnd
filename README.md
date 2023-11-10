575e3095e5337dad76313fb53fd5f39ed20a20e7
Kristy Prieto <kprieto14@gmail.com>
2023-11-09 21:56:58 -0500

    Home page template and outline.

---
+++ README.md
@@ -0,0 +1,3 @@
+A front-end site that allows the user to take care of their pet friends based on a previously made API. Allows you to make a new pet, look at current pets and their own details, play/ feed/ scold your pet, and delete them (but pls don't). Super cute.
+
+Site deployed at the following URL: n/a
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import Kuchipachi from './KuchipatchiFrontFacing.webp'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <h1>Fake Tamagotchi Site</h1>
+      <h2>Choose your pet</h2>
+
+      {/* Pet Menu component here*/}
+      <main>
+        <article>
+          <h3>Pet Name Here</h3>
+          <img src={Kuchipachi} alt="" /> 
+          <button className='pet-button'>Details</button>
+        </article>
+
+        <article>
+          <h3>Pet Name Here</h3>
+          <img src={Kuchipachi} alt="" /> 
+          <button className='pet-button'>Details</button>
+        </article>
+
+        <article>
+          <h3>Pet Name Here</h3>
+          <img src={Kuchipachi} alt="" /> 
+          <button className='pet-button'>Details</button>
+        </article>
+      </main>
+
+      <button>Make a New Pet</button>
+
+      <p>Give photo credits to Ema with link to insta</p>
+
+      <h4>Made with 🫶🏽 in Florida</h4>
+    </div>
+  )
 }
